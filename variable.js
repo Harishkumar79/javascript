d6cde87c74551dbc693994b6c513b4dae9b52e08
DESKTOP-4D80UDC\student <bcghfdhtdf@gmail.com>
2023-01-15 14:43:50 +0530

    list

---
+++ variable.js
@@ -0,0 +1,10 @@
+console.log("hello world")
+var a = 55 //a constant 55
+console.log(a)
+a = "harish"
+console.log(a)
+//let 7harish = 8 //not allowed this will throw an error
+//let var = 8
+//
+//
+//
